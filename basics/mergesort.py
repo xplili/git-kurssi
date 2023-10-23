91e34eae5b860ce6377b27f6baf4ab73aa27c10d
xplili <lina.li@tuni.fi>
2023-10-23 17:39:50 +0300

    mergesort.py

--- basics/mergesort.py
+++ basics/mergesort.py
@@ -8,38 +8,12 @@ def debug_print(debug_msg=None, **kwargs):
         print("{}: {}".format(key, value))
 
 
-def mergesort(array):
-    if len(array) <= 1:
-        return array
-
-    m = len(array) // 2
-
-    left = mergesort(array[:m])
-    right = mergesort(array[m:])
-
-    return merge(left, right)
-
-
-def merge(left, right):
-    merged = []
-
-    while len(left) > 0 and len(right) > 0:
-        if left[0] <= right[0]:
-            merged.append(left.pop(0))
-        else:
-            merged.append(right.pop(0))
-
-    if len(left) > 0:
-        merged += left
-    else:
-        merged += right
-
-    return merged
-
-
 if __name__ == "__main__":
     input_str = input("Enter numbers, separated by ',': ")
+
     input_list = input_str.split(",")
+    debug_print(input_list=input_list)
+
     value_list = []
     for x in input_list:
         try:
@@ -48,5 +22,4 @@ if __name__ == "__main__":
             print("Invalid input.")
             quit(1)
 
-    sorted_list = mergesort(value_list)
-    print(sorted_list)
+    debug_print(value_list=value_list)
